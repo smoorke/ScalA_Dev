78eba29b16e4dad577a720a71bf2d968c54f9eba
smoorke <smoorke@hotmail.com>
2022-01-02 13:37:17 +0100

    Update frmMain.vb

--- ScalA/frmMain/frmMain.vb
+++ ScalA/frmMain/frmMain.vb
@@ -888,8 +888,8 @@
         pnlStartup.ResumeLayout()
     End Sub
     Delegate Sub updateImageDelegate(obj As Object, bm As Bitmap)
-    Private ReadOnly updateImage As updateImageDelegate = New updateImageDelegate(AddressOf updateImageMethod)
-    Private Sub updateImageMethod(obj As Object, bm As Bitmap) 'used by button and toolstripmenuitem
+    Private Shared ReadOnly updateImage As updateImageDelegate = New updateImageDelegate(AddressOf updateImageMethod)
+    Private Shared Sub updateImageMethod(obj As Object, bm As Bitmap) 'used by button and toolstripmenuitem
         If obj Is Nothing Then Exit Sub
         If TypeOf obj Is Button Then
             CType(obj, Button).Image = bm
