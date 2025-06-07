d13df25440d61bbc22d9bc06003b42304319b427
smoorke <smoorke@hotmail.com>
2025-06-07 02:46:40 +0200

    Update frmMain.vb

--- ScalA/frmMain/frmMain.vb
+++ ScalA/frmMain/frmMain.vb
@@ -1928,43 +1928,4 @@ Partial Public NotInheritable Class FrmMain
 
     End Sub
 
-    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
-
-    End Sub
-
-    Private Sub btnstart_MouseDown(sender As Object, e As MouseEventArgs) Handles btnStart.MouseDown
-
-    End Sub
-
-    Private Sub Various_MouseUp(sender As Object, e As MouseEventArgs) Handles pnlSys.MouseUp, pnlButtons.MouseUp, MyBase.MouseUp, ChkEqLock.MouseUp, cboAlt.MouseUp, btnStart.MouseUp, btnQuit.MouseUp
-
-    End Sub
-
-    Private Sub CmbResolution_DropDown(sender As Object, e As EventArgs) Handles cmbResolution.DropDown
-
-    End Sub
-
-    Private Sub CmbResolution_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbResolution.SelectedIndexChanged
-
-    End Sub
-
-    Private Sub ComboBoxes_DropDownClosed(sender As Object, e As EventArgs) Handles cmbResolution.DropDownClosed, cboAlt.DropDownClosed
-
-    End Sub
-
-    Private Sub CmbResolution_MouseUp(sender As Object, e As MouseEventArgs) Handles cmbResolution.MouseUp
-
-    End Sub
-
-    Private Sub cmbResolution_MouseWheel(sender As Object, e As MouseEventArgs) Handles cmbResolution.MouseWheel
-
-    End Sub
-
-    Private Sub CboAlt_DropDown(sender As Object, e As EventArgs) Handles cboAlt.DropDown
-
-    End Sub
-
-    Private Sub CboAlt_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboAlt.SelectedIndexChanged
-
-    End Sub
 End Class
