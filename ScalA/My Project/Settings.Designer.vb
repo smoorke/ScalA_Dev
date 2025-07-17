59ff67d769ed0e26edaf5ac7f8bdc9132ddb9c2d
smoorke <smoorke@hotmail.com>
2025-07-17 14:22:13 +0200

    set togtop to ctrl alt T

--- ScalA/My Project/Settings.Designer.vb
+++ ScalA/My Project/Settings.Designer.vb
@@ -718,7 +718,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("0")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("1")>  _
         Public Property TogTopAlt() As Integer
             Get
                 Return CType(Me("TogTopAlt"),Integer)
@@ -742,7 +742,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("4")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("0")>  _
         Public Property TogTopShift() As Integer
             Get
                 Return CType(Me("TogTopShift"),Integer)
